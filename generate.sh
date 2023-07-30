381a8aac8c6f9391cb358126fd77fa57c2ef02ce
Alex Lauderdale <tzolkat@gmail.com>
2023-07-30 13:21:26 -0400

    Generate now exports vscode format tmlanguage natively.

--- generate.sh
+++ generate.sh
@@ -10,20 +10,21 @@ curl https://raw.githubusercontent.com/ShadowGear-SL/kwdb/dev/lsl2dfg/LSL2dfg.py
 curl https://raw.githubusercontent.com/ShadowGear-SL/kwdb/dev/database/kwdb.xml -o kwdb.xml
 
 # for LSL/OSSL plugin
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_constants -g sl -i inputs/LSLConstants.sublime-completions.in -o "$OUTPUT_DIR/LSLConstants.sublime-completions" -y
+# python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_constants -g sl -i inputs/LSLConstants.sublime-completions.in -o "$OUTPUT_DIR/LSLConstants.sublime-completions" -y
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_constants -g os -i inputs/OSSLConstants.sublime-completions.in -o "$OUTPUT_DIR/OSSLConstants.sublime-completions" -y
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_functions -g sl -i inputs/LSLFunctions.sublime-completions.in -o "$OUTPUT_DIR/LSLFunctions.sublime-completions" -y
+# python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_functions -g sl -i inputs/LSLFunctions.sublime-completions.in -o "$OUTPUT_DIR/LSLFunctions.sublime-completions" -y
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_functions -g os -i inputs/OSSLFunctions.sublime-completions.in -o "$OUTPUT_DIR/OSSLFunctions.sublime-completions" -y
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_events -g sl,os -i inputs/makesnippets.pl.in | perl
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_events -g sl -i inputs/makesnippets.pl.in | perl
+# python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_completions_events -g sl -i inputs/makesnippets.pl.in | perl
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_syntax -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/LSL.sublime-syntax"
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_syntax -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/OSSL.sublime-syntax"
 
 # DEPRECATED - still works but I recommend to create your own scripts if you use this script for other editors
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
+# python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage"
 # python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tmLanguage -g os -t OSSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/ossl.tmLanguage"
+python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f vscode_tmLanguage -g sl -t LSL -i inputs/convertkeywords.pl.in | perl > "$OUTPUT_DIR/lsl.tmLanguage.json"
 
 # for tooltip plugin
-python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tooltip_data -g sl,os -i inputs/tooltipdata.json.in -o "$OUTPUT_DIR_TOOLTIP/tooltipdata.json" -y
+# python lsl2dfg/LSL2dfg.py -u -d kwdb.xml -f sublime_tooltip_data -g sl,os -i inputs/tooltipdata.json.in -o "$OUTPUT_DIR_TOOLTIP/tooltipdata.json" -y
 
 python lsl2dfg/LSL2dfg.py --version --grid=sl,os --database=kwdb.xml
---
+++ lsl2dfg/lsloutputs/vscode_tmLanguage.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sys
+
+def output(document, defaultdescs, databaseversion, infilename, outfilename, lang, tag):
+
+  constants_integer = []
+  constants_string = []
+  constants_float = []
+  constants_compound = []
+  events = []
+  functions = []
+  invalids = []
+  for element in document:
+    if 'status' not in element or element['status'] == 'normal':
+      if element["cat"] == "constant":
+        if element["type"] == "integer":
+          constants_integer.append(element)
+        elif element["type"] == "string":
+          constants_string.append(element)
+        elif element["type"] == "float":
+          constants_float.append(element)
+        elif element["type"] in ["vector", "rotation"]:
+          constants_compound.append(element)
+      elif element["cat"] == "event":
+        events.append(element)
+      elif element["cat"] == "function":
+        functions.append(element)
+    else:
+        invalids.append(element)
+
+  if infilename is not None:
+    inf = open(infilename, "r")
+  else:
+    inf = sys.stdin
+
+  try:
+    inputlines = inf.readlines()
+
+  finally:
+    if infilename is not None:
+      inf.close()
+
+  if outfilename is not None:
+    outf = open(outfilename, "w")
+  else:
+    outf = sys.stdout
+
+  try:
+    for line in inputlines:
+      if line.startswith("<<< INTEGER CONSTANTS >>>"):
+        for entry in constants_integer:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))          
+      elif line.startswith("<<< STRING CONSTANTS >>>"):
+        for entry in constants_string:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))          
+      elif line.startswith("<<< FLOAT CONSTANTS >>>"):
+        for entry in constants_float:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))          
+      elif line.startswith("<<< COMPOUND CONSTANTS >>>"):
+        for entry in constants_compound:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))          
+      elif line.startswith("<<< EVENTS >>>"):
+        for entry in events:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))
+      elif line.startswith("<<< FUNCTIONS >>>"):
+        for entry in functions:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))
+      elif line.startswith("<<< INVALIDS >>>"):
+        for entry in invalids:
+          outf.write("$ra->add( '{0}' );\n".format(entry["name"]))
+      elif line.startswith("<<< VERSION >>>"):
+        outf.write("my $version = '{0}';\n".format(databaseversion))
+        outf.write("my $infile = 'templates/{0}.tmLanguage.json.tmpl';\n".format(tag))
+      else:
+        outf.write(line)
+
+  finally:
+    if outfilename is not None:
+      outf.close()
+
+      # output for this function is perl script.
+      # usually this function is called without outfilename, hence written in sysout.
+      # after then perl will execute the contents in outf.
+
+pass
